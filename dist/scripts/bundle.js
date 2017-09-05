0967d6b6c602c5cfca9817529f5a93e0005f1cb0
Przemysław Owsianik <Przemek.Owsianik@yahoo.com>
2017-09-05 19:49:36 +0200

    Fix for config

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -9213,7 +9213,7 @@ return jQuery;
 },{}],2:[function(require,module,exports){
 $ = jQuery = require('jquery');
 
-var App = console.log('Hello world from Browserify');
+var App = console.log('Hello world');
 
 module.exports = App;
 
